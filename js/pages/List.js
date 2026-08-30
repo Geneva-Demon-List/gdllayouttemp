0431be975a3f69dbb40c80093e9a2616537a8e7e
Rocketpowered-tech <chaserob12345@gmail.com>
2026-08-30 15:33:59 -0400

    Change default selected index from 1 to 0

--- js/pages/List.js
+++ js/pages/List.js
@@ -82,7 +82,7 @@ export default {
         list: [],
         staff: [],
         errors: [],
-        selected: 1,
+        selected: 0,
         store,
         searchQuery: '',
         sortOption: 0,

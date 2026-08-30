2253d114b44b4988cbd77693d21885b50af2c3b1
Rocketpowered-tech <chaserob12345@gmail.com>
2026-08-30 16:42:52 -0400

    Update RecordRules.js

--- js/components/Sidebar/RecordRules.js
+++ js/components/Sidebar/RecordRules.js
@@ -2,57 +2,31 @@ export default {
     template: `
         <h3>Record Submission Requirements</h3>
         <div class="right-text">
-                    <p>
-                        Submission MUST be done by somebody is has been approved to be on the GDL. To be approved, please submit this form. https://forms.gle/UmTFCUXyCYnoF9Yb6
-                    </p>
-                    <p>
-                        Achieved the record without using hacks (however, FPS bypass is allowed). When using mod menus ( Mega Hack, Eclipse, etc ), the Cheat Indicator must be on.
-                    </p>
-                    <p>
-                        Achieved the record on the level that is listed on the site - please check the level ID before you submit a record. If it is a level new to the site, this rule is invalidated.
-                    </p>
-                    <p>
-                        Have audible click sounds ( no clickbot allowed ) OR a handcam throughout the video only IF it is an Insane Demon or Extreme Demon. If it is unrated, it has to be higher than the easiest Rated Insane Demon on the list.
-                    </p>
-                    <p>
-                        About Clickbotting levels, they are considered extremely suspicious on this list. If you clickbot one or two easy demons for example, it will usually not be considered a rulebreak. Doing it often, however, can lead into a purge of your submissions or even a possible ban.
-                    </p>
-                    <p>
-                        The recording must have a previous attempt and entire death animation shown before the completion, unless the completion is on the first attempt.
-                    </p>
-                    <p>
-                        If the level is a verification, a level new to the list, the entire attempt must be recorded. This applies to all levels.
-                    </p>
-                    <p>
-                        For all levels, recorded evidence must be provided. No sending screenshots because they do not provide enough evidence for it to be legit.
-                    </p>
-                    <p>
-                        The recording must also show the player hit the endwall, or the completion will be invalidated.
-                    </p>
-                    <p>
-                        Do not use secret routes or bug routes
-                    </p>
-                    <p>
-                        Do not use easy modes, only a record of the unmodified level qualifies
-                    </p>
-                    <p>
-                        All levels on this list must be published and publicly available for anybody with the full version to play.
-                    </p>
-                    <p>
-                        All submissions on the Main list on levels less than 30 seconds long must go to the challenge list. You can check this by seeing the level page, and if it is described as "Short" or "Tiny", it belongs on the Challenge List.
-                    </p>
-                    <p>
-                        All submissions must be done on a level already on the list, or if it is a GDL Verification, the level must either be rated, be decorated to some extent, or have historial significance.
-                    </p>
-                    <p>
-                        Non-Demon levels are not allowed on this list, the minimum are Easy Demon difficulty levels.
-                    </p>
-                    <p>
-                        If you use Mega Hack, you must set your ruleset to Demonlist. This also means that if the GD Demonlist does not allow a hack to be turned on, the GDL doesn't either.
-                    </p>
-                    <p>
-                        If one were to beat a level on this list on their own personal copy ( StartPos copy, for example ), they must upload it and have it reviewed by the mods. The personal copy should not make the level any easier or harder to play, this involves, nerfs, buffs, or changing the decoration. ( No layouts! )
-                    </p>
+            <p>
+                You must have achieved the record without using hacks (including hacks that change the physics of the game, ie. physics bypass via MegaHack. Note that Click Between Frames is allowed but its Physics Bypass setting is banned.).
+            </p>
+            <p>
+                You must have achieved the record on the level that is listed on the site or on an approved bugfixed copy - please check the level ID before you submit a record!
+            </p>
+            <p>
+                Records for Hard Demon+ layouts must have audible clicks for the entire completion attempt. This does not include mods that add artificial click sounds.
+            </p>
+            <p>
+                Complete raw footage is required alongside your record for any levels at Sonic Wave difficulty or above.
+            </p>
+            <p>
+                The recording must have a previous attempt and death animation shown before the completion, unless the completion is on the first attempt.
+            </p>
+            <p>
+                The recording must show the player hit the end-wall as well as present your end stats, or the completion will be invalidated.
+            </p>
+            <p>
+                Do not use secret routes, skips, or bug routes!
+            </p>
+            <p>
+                Cheat Indicator is required for all completions via MegaHack, Geode, or iCreate Pro. If you do not have Cheat Indicator on, your record will likely be invalidated (this is not 100% required for mobile as of yet due to mobile limitations).
+                If you are unsure which Geode mod menu to use, we recommend Eclipse.
+            </p>
         </div>
     `
 }

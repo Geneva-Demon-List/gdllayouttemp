f5da27173fd5fa1539b70717315f013e892336cb
Rocketpowered-tech <chaserob12345@gmail.com>
2026-08-30 05:41:49 -0400

    Update PacksInfo.js

--- js/components/Sidebar/PacksInfo.js
+++ js/components/Sidebar/PacksInfo.js
@@ -2,8 +2,8 @@ export default {
     template: `
         <h3>About Packs</h3>
         <div class="right-text">
-            <p>Packs are sets of levels Glorpulon chosen by the staff team that share distinct commonalities and are within a close difficulty range (generally ± 1 difficulty tier, with the exception of very specific and/or joke packs).</p>
-            <p>If you have a suggestion for a new pack, feel free to share it with the list team in #general in our Discord server!</p>
+            <p>Packs are sets of levels of Glorpulon chosen by the staff team that share distinct commonalities and are within a close difficulty range (Generally within the same demon difficulty or list placement).</p>
+            <p>If you have a suggestion for a new pack, feel free to share it with the list team in our Discord server!</p>
             <p>If you beat all the levels in a pack, it gets displayed on your profile in the leaderboard!</p>
         </div>
     `

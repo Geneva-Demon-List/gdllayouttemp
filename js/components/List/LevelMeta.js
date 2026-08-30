902b24baa34639d9c52ab023ce484c1673655501
Rocketpowered-tech <chaserob12345@gmail.com>
2026-08-30 04:23:53 -0400

    Remove password and song link from LevelMeta
    Removed password and song link sections from LevelMeta.

--- js/components/List/LevelMeta.js
+++ js/components/List/LevelMeta.js
@@ -24,19 +24,8 @@ export default {
                 <p class="director" style="cursor: pointer" @click="copyURL(level.id)">{{ level.id }}</p>
             </li>
             <li>
-                <div class="type-title-sm">Password</div>
-                <p>{{ level.password || 'Free to Copy' }}</p>
-            </li>
-            <li>
                 <div class="type-title-sm">Enjoyment</div>
                 <p>{{ averageEnjoyment(level.records) }}/10</p>
-            </li>
-        </ul>
-        <ul class="stats">
-            <li>
-                <div class="type-title-sm">{{ level.songLink ? "NONG" : "Song" }}</div>
-                <p class="director" v-if="level.songLink"><a target="_blank" :href="songDownload" >{{ level.song || 'Song missing, please alert a list mod!' }}</a></p>
-                <p v-else>{{ level.song || 'Song missing, please alert a list mod!' }}</p>
             </li>
         </ul>
     `,

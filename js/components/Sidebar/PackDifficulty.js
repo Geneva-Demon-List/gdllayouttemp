50afdc5183384da24d2b2c0021bb98532b491c50
Rocketpowered-tech <chaserob12345@gmail.com>
2026-08-30 20:47:57 -0400

    Update PackDifficulty.js

--- js/components/Sidebar/PackDifficulty.js
+++ js/components/Sidebar/PackDifficulty.js
@@ -2,7 +2,14 @@ export default {
     template: `
         <h3>Difficulty Rankings</h3>
         <div class="right-text">
-            <p>They're cosemetic buddy</p>
+            <p>Packs do not give points, but their difficulty ranking is based off of what difficulty they mostly consist of, mostly biased by the hardest level in the pack.</p>
+            <p>Beginner: Mostly Tier 1-2 Easy Demons</p>
+            <p>Easy: Mostly Tier 3-5 Easy Demons</p>
+            <p>Medium: Mostly Medium Demons</p>
+            <p>Hard: Mostly Hard Demons</p>
+            <p>Insane: Mostly Insane Demons</p>
+            <p>Mythical: Mostly Easy Extreme Demons ( Bloodbath and Easier )</p>
+            <p>Extreme: Mostly Extreme Demons ( Harder than Bloodbath )</p>
         </div>
     `
 }

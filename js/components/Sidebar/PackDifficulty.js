1214cb31da247fb04d4842f9df9a92ca5c274721
Rocketpowered-tech <chaserob12345@gmail.com>
2026-09-04 16:27:56 -0400

    Revise difficulty descriptions with GDDL tiers
    Updated difficulty rankings with GDDL tier references.

--- js/components/Sidebar/PackDifficulty.js
+++ js/components/Sidebar/PackDifficulty.js
@@ -3,10 +3,10 @@ export default {
         <h3>Difficulty Rankings</h3>
         <div class="right-text">
             <p>Packs do not give points, but their difficulty ranking is based off of what difficulty they mostly consist of, mostly biased by the hardest level in the pack.</p>
-            <p>Beginner: Mostly Tier 1-2 Easy Demons</p>
-            <p>Easy: Mostly Tier 3-5 Easy Demons</p>
-            <p>Medium: Mostly Medium Demons</p>
-            <p>Hard: Mostly Hard Demons</p>
+            <p>Beginner: Mostly Very Easy Demons ( GDDL Tier 1-2 Difficulty )</p>
+            <p>Easy: Mostly Easy Demons ( GDDL Tier 3-5 Difficulty )</p>
+            <p>Medium: Mostly Medium Demons ( GDDL Tier 6-10 Difficulty )</p>
+            <p>Hard: Mostly Hard Demons ( GDDL Tier 11-15 Difficulty )</p>
             <p>Insane: Mostly Low Insane Demons ( GDDL Tier 16-18 Difficulty )</p>
             <p>Mythical: Mostly High Insane Demons ( GDDL Tier 19-21 Difficulty )</p>
             <p>Extreme: Mostly Extreme Demons ( GDDL Tier 21-25 Difficulty )</p>

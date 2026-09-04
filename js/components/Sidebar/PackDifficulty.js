c74e182897c83380c976419abcd18e025e2e6335
Rocketpowered-tech <chaserob12345@gmail.com>
2026-09-04 16:23:40 -0400

    Update difficulty descriptions in PackDifficulty.js

--- js/components/Sidebar/PackDifficulty.js
+++ js/components/Sidebar/PackDifficulty.js
@@ -7,9 +7,10 @@ export default {
             <p>Easy: Mostly Tier 3-5 Easy Demons</p>
             <p>Medium: Mostly Medium Demons</p>
             <p>Hard: Mostly Hard Demons</p>
-            <p>Insane: Mostly Insane Demons</p>
-            <p>Mythical: Mostly Easy Extreme Demons ( Bloodbath and Easier )</p>
-            <p>Extreme: Mostly Extreme Demons ( Harder than Bloodbath )</p>
+            <p>Insane: Mostly Low Insane Demons ( GDDL Tier 16-18 Difficulty )</p>
+            <p>Mythical: Mostly High Insane Demons ( GDDL Tier 19-21 Difficulty )</p>
+            <p>Extreme: Mostly Extreme Demons ( GDDL Tier 21-25 Difficulty )</p>
+            <p>Legendary: Mostly Hard Extreme Demons ( GDDL Tier 26+ Difficulty )</p>
         </div>
     `
 }
